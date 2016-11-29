bf3e8eda442105e7a11fcfb41aaaec05d866bca2
TingKong <kongsau@gmail.com>
2016-11-29 11:40:29 -0500

    uploading initial files

---
+++ TradingServices/TradingServices/Models/Jobs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TradingServices.Models
+{
+    public class Jobs
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+
+    }
+}
---
+++ TradingServices/TradingServices/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TradingServices.Startup))]
+namespace TradingServices
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
